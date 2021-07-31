f1a9ce0fdaf75361e2f9a1e0e3d13cae28a9a4e2
zuzuviewer <750938164@qq.com>
2021-07-31 11:29:07 +0800

    Add output string json data function
    add flag 'string'(s) to output string format json data

--- cmd/command/convert.go
+++ cmd/command/convert.go
@@ -19,8 +19,10 @@ var (
 	sourceFile string
 	// output,default is empty,output to stdout
 	dest string
-	// is output prettified json data,if it's true,only execute prettified data
+	// is output prettified json data,if it's true,only execute prettified data and only accept json format data
 	indent bool
+	// is output string format json data,if it's true,only output string format json data and only accept json format data
+	outStr bool
 )
 
 func init() {
@@ -28,6 +30,7 @@ func init() {
 	convertCmd.MarkFlagRequired("file")
 	convertCmd.Flags().StringVarP(&dest, "write", "w", "", "dest to output")
 	convertCmd.Flags().BoolVarP(&indent, "indent", "i", false, "output prettified json")
+	convertCmd.Flags().BoolVarP(&outStr, "string", "s", false, "output string format json")
 }
 
 var convertCmd = &cobra.Command{
@@ -37,8 +40,11 @@ var convertCmd = &cobra.Command{
 		"'*.json' file will be treat as json file and convert to yaml data,'*.yml' and '*.yaml' will be treat as yaml file and convert to json data\n" +
 		"if file name doesn't have these extension name,Convert will try convert json to yaml first,if error occurred,then try convert yaml to json\n" +
 		"the result output to stdout default,otherwise output to the dest if execute with -w.\n" +
-		"prettified json output with -i or --indent,and now only accept json data and won't convert data to yaml\n",
-	Example: "openk convert -f test.json\nopenk convert -f test.yml -w test.json\nopenk convert -i -f json.txt\nopenk convert -i -f json.txt -w t.json\n",
+		"prettified json output with -i or --indent,and now only accept json data and won't convert data to yaml.\n" +
+		"output string format json data with -s or --string.\n" +
+		"flag 'i'(indent) and 's'(string) only accept json data.\n",
+	Example: "openk convert -f test.json\nopenk convert -f test.yml -w test.json\n" +
+		"openk convert -i -f json.txt\nopenk convert -i -f json.txt -w t.json\n",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return convert()
 	},
@@ -49,14 +55,27 @@ func convert() error {
 	if err != nil {
 		return err
 	}
-	// only do prettified json data
-	if indent {
-		data, err = indentJson(data)
-		if err != nil {
+	if indent || outStr {
+		ok := json.Valid(data)
+		if !ok {
+			return fmt.Errorf("invaild json input data")
+		}
+		// only do prettified json data
+		if indent {
+			data, err = indentJson(data)
+			if err != nil {
+				return err
+			}
+			return output(data)
+		}
+		// only output string format json data
+		buf := bytes.NewBuffer(make([]byte, 0))
+		if err = json.Compact(buf, data); err != nil {
 			return err
 		}
-		return output(data)
+		return output(buf.Bytes())
 	}
+
 	if strings.HasSuffix(sourceFile, ".json") {
 		if data, err = yaml.JSONToYAML(data); err != nil {
 			return err
